5b94c2de2242a01bf0aab791c1bfc19860b44384
hailh <black_cat_hh@yahoo.com>
2013-08-17 10:40:05 +0700

    Unit test checkFinishGame - win in column - GREEN

--- UnitTestTicTacToe/web-app/app-test/ui-test/common/utils.js
+++ UnitTestTicTacToe/web-app/app-test/ui-test/common/utils.js
@@ -13,7 +13,26 @@ Utils = {
     },
 
     checkFinishGame: function () {
-        return false;
+        var isFinish = false;
+        isFinish = Utils.checkWinInColumn();
+        return isFinish;
+    },
+
+    checkWinInColumn: function () {
+        var finish = true;
+        for(var i = 0; i < 3; i++){
+            finish = true;
+            var state = Utils.board[0][i];
+            for(var j =0; j < 3; j++){
+                console.log(Utils.board[j][i]);
+                if(Utils.board[j][i] != state || Utils.board[j][i] == 0){
+                    finish = false;
+                }
+                if(finish == true && j == 2){
+                    return true;
+                }
+            }
+        }
     }
 }
 
